66d8cb886a258bb8ef54280a0c6b354620a2df9e
edwardfang1945 <edwardfang1945@outlook.com>
2020-09-15 19:08:26 +0800

    Update Python version 3.7.9

--- packaging/cura.sh
+++ packaging/cura.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-scriptdir=$(dirname $0)
+scriptdir="$(dirname $0)"
 
-export PYTHONPATH="$scriptdir/lib/python3.5"
-export QT_PLUGIN_PATH="$scriptdir/qt/plugins"
-export QML2_IMPORT_PATH="$scriptdir/qt/qml"
-export QT_QPA_FONTDIR=/usr/share/fonts
-export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
+export PYTHONPATH=$scriptdir/../lib
+#export QT_PLUGIN_PATH=$scriptdir/qt/plugins
+export QML2_IMPORT_PATH=$scriptdir/../lib/UM/Qt/qml
+#export QT_QPA_FONTDIR=/usr/share/fonts
+#export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
 
 # Use the openssl.cnf packaged in the AppImage
-export OPENSSL_CONF="$scriptdir/openssl.cnf"
+#export OPENSSL_CONF=$scriptdir/openssl.cnf
 
-cura "$@"
+"$scriptdir/../cura" "$@"
